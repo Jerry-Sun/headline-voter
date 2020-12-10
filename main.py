042a8117a4077112d473e67c35ed7df892a84ae4
Jerry <jiarsun@indiana.edu>
2020-12-09 20:35:48 -0800

    minor changes to page layout

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ import pprint, json, requests
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# Read the JSON data from the file into the program
 with open("example2.json", "r") as f: 
     data = json.loads(f.read())
 
-#@app.get("/")
-#async def homepage(request: Request):
-#    return templates.TemplateResponse("index.html", {"request": request})
+# Homepage when you visit the localhost site
+@app.get("/")
+async def homepage(request: Request):
+    return {"message": "Welcome to this MediaStack project"}
 
+# Webpage when you visit the "localhost/jinja" url
 @app.get("/jinja")
 async def get_headlines(request: Request):
 #    print(data["data"])
