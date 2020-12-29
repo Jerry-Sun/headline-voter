ac91a8eebae643a77839a7890ab7fdf57cfbddd1
jerry-sun <jerry.jn.sun@gmail.com>
2020-12-29 12:01:53 -0800

    change API parameters

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ url = "http://api.mediastack.com/v1/news"
 PARAMS = {
    "access_key" : "ab0622cf5843c7ae44b724303b6ef796",
    "categories": "business, technology, -entertainment",
-   "country" : "us",
-   "language" : "en",
+   "countries" : "us",
+   "languages" : "en",
    "sort" : "published_desc",
    "limit" : 10,
 }
